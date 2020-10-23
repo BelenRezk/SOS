73ac4723723639be10a8b7a91cc8f69c5c472e28
BelenRezk <48623962+BelenRezk@users.noreply.github.com>
2020-10-23 19:46:05 -0300

    AI random path

---
+++ SOS/Assets/AIPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIPath : MonoBehaviour
+{
+    public Transform[] path;
+    NavMeshAgent _navMeshAgent;
+    private int randomDestination;
+
+    void Start()
+    {
+        randomDestination = (int)Random.Range(0f, path.Length);
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+        _navMeshAgent.destination = path[randomDestination].position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AIView obj = gameObject.GetComponentInChildren<AIView>();
+        var dist = Vector3.Distance(path[randomDestination].position, _navMeshAgent.transform.position);
+        if (dist < 1.5 || path[randomDestination].parent != null)
+        {
+            randomDestination = (int)Random.Range(0f, path.Length);
+            _navMeshAgent.destination = path[randomDestination].position;
+        }
+    }
+}
---
+++ SOS/Assets/Scripts/AI scripts/AIView.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIView : MonoBehaviour
+{
+    public Dictionary<string, Vector3> PlayersPossitions { get; set; }
+    public Dictionary<string, Vector3> ItemsPossitions { get; set; }
+
+
+    private void Start()
+    {
+        PlayersPossitions = new Dictionary<string, Vector3>();
+        ItemsPossitions = new Dictionary<string, Vector3>();
+
+    }
+
+    private void OnTriggerStay(Collider other) {
+        bool isPlayer = other.CompareTag("Player");
+        if (isPlayer)
+        {
+            //ATTACK PLAYER
+            if (PlayersPossitions.ContainsKey(other.name))
+            {
+                PlayersPossitions[other.name] = other.transform.position;
+                Debug.Log("AGREGO PLAYER AL ");
+            }
+            else
+            {
+                PlayersPossitions.Add(other.name, other.transform.position);
+                Debug.Log("COLISION CON PLAYER");
+            }
+        }
+        bool isItem = other.CompareTag("Item");
+        if (isItem)
+        {
+            //GO TO OBJECT
+            if (ItemsPossitions.ContainsKey(other.name))
+                ItemsPossitions[other.name] = other.transform.position;
+            else
+                ItemsPossitions.Add(other.name, other.transform.position);
+            Debug.Log("COLISION CON ITEM");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        bool isPlayer = other.CompareTag("Player");
+        if (isPlayer && PlayersPossitions.ContainsKey(other.name))
+        {
+            PlayersPossitions.Remove(other.name);
+            Debug.Log("SALIO DE COLISION CON PLAYER");
+        }
+        bool isItem = other.CompareTag("Item");
+        if (isItem && ItemsPossitions.ContainsKey(other.name))
+        {
+            ItemsPossitions.Remove(other.name);
+            Debug.Log("SALIO DE COLISION CON ITEM");
+        }
+    }
+
+}
