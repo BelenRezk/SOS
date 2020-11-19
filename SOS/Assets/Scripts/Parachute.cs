5a7fa272089fddaa2b3355e5e425366444fdab7c
noelapence <noepence@hotmail.com>
2020-11-18 21:53:43 -0300

    Added parachute and emergency kit scripts

---
+++ SOS/Assets/Scripts/Parachute.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parachute : InventoryItemBase
+{
+    public override string Name
+    {
+        get
+        {
+            return "Parachute";
+        }
+    }
+
+    public override bool WinItem
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+    public override bool OnUse()
+    {
+        return false;
+    }
+}
