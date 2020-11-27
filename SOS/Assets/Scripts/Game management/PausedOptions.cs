0648f9ace7e0fcb4ccd498018e532a4ce00ceff4
noelapence <noepence@hotmail.com>
2020-11-27 14:22:43 -0300

    Added paused menu options

---
+++ SOS/Assets/Scripts/Game management/PausedOptions.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausedOptions : MonoBehaviour
+{
+    [SerializeField] public GameObject pauseMenuUI;
+    [SerializeField] public GameObject pausePanel;
+    [SerializeField] public GameObject instructionsPanel;
+    [SerializeField] public GameObject keyMappingPanel;
+    [SerializeField] public bool isPaused;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }        
+        if(isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+    
+    void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        DeactivateMenu();
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("menuScene");
+    }
+
+    public void KeyMappingMenu()
+    {
+        pausePanel.SetActive(false);
+        keyMappingPanel.SetActive(true);
+    }
+
+    public void KeyMappingToPausedMenu()
+    {
+        pausePanel.SetActive(true);
+        keyMappingPanel.SetActive(false);
+    }
+
+    public void InstructionsMenu()
+    {
+        pausePanel.SetActive(false);
+        instructionsPanel.SetActive(true);
+    }
+    
+    public void InstructionsToPausedMenu()
+    {
+        instructionsPanel.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+}
