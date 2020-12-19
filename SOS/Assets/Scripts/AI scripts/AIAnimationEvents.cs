76fd89b0e1f1b92b7be67fbc15a5dd044683e51c
noelapence <noepence@hotmail.com>
2020-12-18 21:22:41 -0300

    Fixed stuck ai animations

--- SOS/Assets/Scripts/AI scripts/AIAnimationEvents.cs
+++ SOS/Assets/Scripts/AI scripts/AIAnimationEvents.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class AIAnimationEvents : MonoBehaviour
 {
+    private void Update() 
+    {
+        if(this.GetComponent<Animator>().GetBool("IsWalking") && this.GetComponent<Animator>().GetBool("ThrowingCoconut") && this.GetComponent<Animator>().GetBool("WasHit"))
+        {
+            this.GetComponent<Animator>().SetBool("ThrowingCoconut", false);
+            this.GetComponent<Animator>().SetBool("WasHit", false);
+        }    
+    }
 
     public void ThrowCoconut()
     {
